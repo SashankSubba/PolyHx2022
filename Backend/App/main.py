d003f00b1790c4a1f4ebf46e24a9ae83a3e347cf
Sashank Subba <s_ubba@encs.concordia.ca>
2022-02-12 16:21:12 -0500

    Modified main.py login route

--- Backend/App/main.py
+++ Backend/App/main.py
@@ -1,10 +1,10 @@
 from db import db_connect, auth_user
-from flask import Flask, request, redirect, url_for, flash
+from flask import Flask, request, redirect, url_for, flash, session
 from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"*": {"origins": "*"}})
 
 
 @app.route("/")
@@ -15,16 +15,24 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     conn = db_connect()
+    error = None
 
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        user = auth_user(conn, username, password)
+        user = auth_user(conn, username, password).fetchone()
 
-        if user:
+        if user is None:
+            error = 'Incorrect Login'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
             return redirect(url_for('index'))
-        else:
-            flash("failure")
+
+        flash(error)
+
+    return "<p>Hello, World!</p>"
 
 
 if __name__ == '__main__':
