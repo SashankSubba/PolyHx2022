2ca4786f5d0d9fc900c94c019a205d9a091bbbfb
Sashank Subba <s_ubba@encs.concordia.ca>
2022-02-12 15:47:19 -0500

    Added login route

--- Backend/App/main.py
+++ Backend/App/main.py
@@ -1,11 +1,10 @@
-from flask import Flask, request
+from db import db_connect, auth_user
+from flask import Flask, request, redirect, url_for, flash
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///guardianDB.sqlite'
 CORS(app)
-db = SQLAlchemy(app)
 
 
 @app.route("/")
@@ -15,25 +14,18 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    conn = db_connect()
+
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+        user = auth_user(conn, username, password)
 
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), unique=False, nullable=False)
-
-    def __init__(self, id, username, password):
-        self.id = id
-        self.username = username
-        self.password = password
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+        if user:
+            return redirect(url_for('index'))
+        else:
+            flash("failure")
 
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True)
--- Backend/schema.sql
+++ Backend/schema.sql
@@ -5,3 +5,5 @@ CREATE TABLE user (
   username TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL
 );
+
+INSERT INTO user (username, password) VALUES ('Test User', 'Test Password');
