304baa8674a7509af1b544558720c1f1fbdafd1a
son-tmg <27878892+son-tmg@users.noreply.github.com>
2022-02-12 03:59:46 -0500

    Added login form and logo

--- Frontend/src/pages/login.vue
+++ Frontend/src/pages/login.vue
@@ -1,13 +1,57 @@
 <template>
-  <div >
+  <div>
       <h1>Welcome to Guardians !</h1>
+    <img src=../assets/logo.jpg width="10%">
+    <b-form onsubmit="submit" class="mt-5">
+        <b-row>
+          <b-col cols="5">
+            <label>Email</label>
+          </b-col>
+          <b-col cols="5">
+            <b-form-input
+              v-model="email"
+              placeholder="Enter your email">
+            </b-form-input>
+          </b-col>
+        </b-row>
+
+        <b-row>
+          <b-col cols="5">
+            <label>Password</label>
+          </b-col>
+          <b-col cols="5">
+            <b-form-input
+              v-model="password"
+              placeholder="Enter your email">
+            </b-form-input>
+          </b-col>
+        </b-row>
+
+      <b-row class="mt-5">
+        <b-col cols="4"></b-col>
+        <b-col cols="4">
+          <b-button style="margin-right: 20px">Login</b-button>
+          <a href="signup.vue">Create Profile</a>
+        </b-col>
+        <b-col cols="4"></b-col>
+      </b-row>
+    </b-form>
   </div>
 </template>
 
 <script>
 
 export default {
-    name:"login"
+    name:"login",
+    data(){
+        return {
+          email : "",
+          password: ""
+        }
+    },
+    methods: {
+
+    }
 }
 </script>
 
