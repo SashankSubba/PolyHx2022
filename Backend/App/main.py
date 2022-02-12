b4ca425a1eb1e09e21fa0b7e3e4c6301dfecc7bc
Sashank Subba <s_ubba@encs.concordia.ca>
2022-02-12 13:41:28 -0500

    Added models.py for user model and db initialization with flask-sqlalchemy

--- Backend/App/main.py
+++ Backend/App/main.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from models import User
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 CORS(app)
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
     return "<p>Hello, World!</p>"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+if __name__ == '__main__':
+   db.create_all()
+   app.run(debug = True)
